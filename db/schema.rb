f39b8eb68acb4f14fc17815db27ea5a0830abd90
RyujiTakami <r.takami@and1acct.jp>
2019-02-05 19:32:22 +0900

    Membersテーブルを作成
    Membersテーブルを作成した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205100939) do
+ActiveRecord::Schema.define(version: 20190205102258) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id_id",  null: false
+    t.integer  "group_id_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["group_id_id"], name: "index_members_on_group_id_id", using: :btree
+    t.index ["user_id_id"], name: "index_members_on_user_id_id", using: :btree
   end
 
 end

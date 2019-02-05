dbe4767a580fc25948874455880db28c8d33d68b
RyujiTakami <r.takami@and1acct.jp>
2019-02-05 21:24:44 +0900

    Usersテーブル修正
    Usersテーブル修正した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205103604) do
+ActiveRecord::Schema.define(version: 20190205110008) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 20190205103604) do
     t.datetime "updated_at",                null: false
     t.index ["group_id_id"], name: "index_messeges_on_group_id_id", using: :btree
     t.index ["user_id_id"], name: "index_messeges_on_user_id_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name",                   default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end

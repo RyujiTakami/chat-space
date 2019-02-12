8aa3ef8c7558ab9c22626637a2ddd70075c3fd9a
RyujiTakami <r.takami@and1acct.jp>
2019-02-12 21:51:29 +0900

    User Deviseの追加と修正
    User Deviseの追加と修正した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205140043) do
+ActiveRecord::Schema.define(version: 20190212110244) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -36,29 +36,6 @@ ActiveRecord::Schema.define(version: 20190205140043) do
     t.datetime "updated_at",                null: false
     t.index ["group_id_id"], name: "index_messages_on_group_id_id", using: :btree
     t.index ["user_id_id"], name: "index_messages_on_user_id_id", using: :btree
-  end
-
-  create_table "messeges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",        limit: 65535
-    t.string   "image"
-    t.integer  "user_id_id",                null: false
-    t.integer  "group_id_id",               null: false
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.index ["group_id_id"], name: "index_messeges_on_group_id_id", using: :btree
-    t.index ["user_id_id"], name: "index_messeges_on_user_id_id", using: :btree
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end

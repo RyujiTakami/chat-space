f684af4c206daf7845fc7d4524824c4f1754568a
RyujiTakami <r.takami@and1acct.jp>
2019-02-14 22:09:17 +0900

    Side-barに所属するグループ一覧を表示
    Side-barに所属するグループ一覧を表示した

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users,   only: [:update, :edit]
-  resources :groups,  only: [:new, :edit, :create, :update, :delete] do |variable|
+  resources :groups,  only: [:index , :new, :edit, :create, :update, :delete] do |variable|
     resources :members,  only: [:index, :create]
   end
 end

a021520816d4967f9a77a61b6462935bc0e0e14a
RyujiTakami <r.takami@and1acct.jp>
2019-02-14 23:54:54 +0900

    Groupの新規作成と編集画面の部分テンプレート化
    Groupの新規作成と編集画面の部分テンプレート化しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  root "groups#index"
   resources :users,   only: [:update, :edit]
   resources :groups,  only: [:index , :new, :edit, :create, :update, :delete] do |variable|
     resources :members,  only: [:index, :create]

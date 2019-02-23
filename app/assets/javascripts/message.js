a804ef06fcd8f5875fb46b0c56182902f4319c24
RyujiTakami <r.takami@and1acct.jp>
2019-02-24 04:13:52 +0900

    Jsの非同期通信部分
    Jsの非同期通信部分です

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image !== null) {
+    var html = `
+              <div class="message__box">
+                <div class="message__box--user-name">
+                  ${message.user_name}
+                  <span class="message__box--user-name-date">
+                    ${message.created_at}
+                 </span>
+                </div>
+                <div class="message__box--message">
+                  <div class="message__box--message-content">
+                    ${ message.content}
+                  </div>
+                  <img src="${message.image}", class="message__box--message-image">
+                </div>
+              </div>`
+
+                } else{
+
+    var html = `
+              <div class="message__box">
+                <div class="message__box--user-name">
+                  ${message.user_name}
+                  <span class="message__box--user-name-date">
+                    ${message.created_at}
+                  </span>
+                </div>
+                <div class="message__box--message">
+                  <div class="message__box--message-content">
+                    ${ message.content}
+                  </div>
+                </div>
+              </div>`
+                    }
+
+    return html
+  }
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action")
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".messages").append(html)
+      $("#message_content").val("")
+      $(".messages").animate({"scrollTop": "100000px"})
+    })
+
+    .fail(function() {
+      alert("error");
+    })
+
+  })
+});

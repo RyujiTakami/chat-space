903953694687734fa97a852847d0b15f01c9a010
RyujiTakami <r.takami@and1acct.jp>
2019-02-26 00:12:26 +0900

    Users/indexのviewを使用
    Users/indexのviewを使用したルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "groups#index"
-  resources :users,   only: [:update, :edit]
+  resources :users,   only: [:index, :update, :edit]
   resources :groups,  only: [:index , :new, :edit, :create, :update, :delete] do
     resources :messages,  only: [:index, :create]
   end

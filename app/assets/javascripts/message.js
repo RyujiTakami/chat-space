bb2e8eb0fa7ce3a647ef7ab3da3b8286eadef11c
RyujiTakami <r.takami@and1acct.jp>
2019-02-27 02:29:56 +0900

    自動更新化
    自動更新化しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if (message.image !== null) {
     var html = `
-              <div class="message__box">
+              <div class="message__box" data-id=${message.id}>
                 <div class="message__box--user-name">
                   ${message.user_name}
                   <span class="message__box--user-name-date">
@@ -20,7 +20,7 @@ $(function(){
                 } else{
 
     var html = `
-              <div class="message__box">
+              <div class="message__box" data-id=${message.id}>
                 <div class="message__box--user-name">
                   ${message.user_name}
                   <span class="message__box--user-name-date">
@@ -65,4 +65,78 @@ $(function(){
     })
 
   })
+
+
+    $(function(){
+
+      setInterval(update,5000);
+    });
+
+      var message_list = $(".messages")
+
+      function buildMESSAGE(message){
+        if (message.image !== null) {
+        var html = `
+                  <div class="message__box" data-id=${message.id}>
+                    <div class="message__box--user-name">
+                      ${message.user_name}
+                      <span class="message__box--user-name-date">
+                        ${message.created_at}
+                     </span>
+                    </div>
+                    <div class="message__box--message">
+                      <div class="message__box--message-content">
+                        ${ message.content}
+                      </div>
+                      <img src="${message.image}", class="message__box--message-image">
+                    </div>
+                  </div>`
+
+                    } else{
+
+        var html = `
+                  <div class="message__box" data-id=${message.id}>
+                    <div class="message__box--user-name">
+                      ${message.user_name}
+                      <span class="message__box--user-name-date">
+                        ${message.created_at}
+                      </span>
+                    </div>
+                    <div class="message__box--message">
+                      <div class="message__box--message-content">
+                        ${ message.content}
+                      </div>
+                    </div>
+                  </div>`
+                        }
+
+        message_list.append(html)
+      }
+
+    function update(){
+      if ($(".message__box")[0]) {
+        var message_id = $(".message__box:last").data("id");
+      } else {
+        var message_id = 0
+      }
+      $.ajax({
+        url: window.location.href,
+        type: "GET",
+        data: {
+          message: { id: message_id }
+        },
+        dataType: "json"
+      })
+
+      .always(function(data){
+        console.log(data)
+        $.each(data, function(i, data){
+        buildMESSAGE(data);
+        $(".messages").animate({"scrollTop": "100000px"})
+
+        });
+      });
+
+    }
+
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,10 @@ class MessagesController < ApplicationController
     @message = Message.new
     @messages = @group.messages.includes(:user)
     @members = @group.users
+    respond_to do |format|
+      format.html
+      format.json {@new_message = Message.where("id > ?", params[:message][:id])}
+    end
   end
 
   def create

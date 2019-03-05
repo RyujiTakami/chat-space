c3333716efe03db8fc9b05bb10572100f365ce8a
RyujiTakami <r.takami@and1acct.jp>
2019-03-05 23:42:10 +0900

    Carrier wave.rb
    Carrier wave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'ryuji-takami-test'
-  config.asset_host = 'https://s3-ここにリージョン名を入れます(※例、us-east-1).amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ryuji-takami-test'
 end
